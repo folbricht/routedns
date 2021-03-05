e81e86b34ae802fab25a0aeca427da69c244f0dc
Frank Olbricht <frank.olbricht@gmail.com>
2021-03-04 19:50:42 -0700

    Return copies of responses from the cache (#136)
    * Return a copy of the response from the cache
    
    * Add comment
    
    * Also protect cached items against query modifications

--- cache.go
+++ cache.go
@@ -107,7 +107,7 @@ func (r *Cache) Resolve(q *dns.Msg, ci ClientInfo) (*dns.Msg, error) {
 	log.WithField("resolver", r.resolver.String()).Debug("cache-miss, forwarding")
 
 	// Get a response from upstream
-	a, err := r.resolver.Resolve(q, ci)
+	a, err := r.resolver.Resolve(q.Copy(), ci)
 	if err != nil || a == nil {
 		return nil, err
 	}
@@ -160,6 +160,10 @@ func (r *Cache) answerFromCache(q *dns.Msg) (*dns.Msg, bool) {
 	if answer == nil {
 		return nil, false
 	}
+
+	// Make a copy of the response before returning it. Some later
+	// elements might make changes.
+	answer = answer.Copy()
 	answer.Id = q.Id
 
 	// Calculate the time the record spent in the cache. We need to
