578ae7a74862789191461b95b5db3d95edf4b0d7
folbrich <frank.olbricht@gmail.com>
2024-09-29 13:20:56 +0200

    use slog to produce logs

--- query-log.go
+++ query-log.go
@@ -1,11 +1,9 @@
 package rdns
 
 import (
-	"fmt"
-	"io"
+	"context"
+	"log/slog"
 	"os"
-	"sync"
-	"time"
 
 	"github.com/miekg/dns"
 )
@@ -15,8 +13,7 @@ type QueryLogResolver struct {
 	id       string
 	resolver Resolver
 	opt      QueryLogResolverOptions
-	w        io.Writer
-	mu       sync.Mutex
+	logger   *slog.Logger
 }
 
 var _ Resolver = &QueryLogResolver{}
@@ -35,23 +32,43 @@ func NewQueryLogResolver(id string, resolver Resolver, opt QueryLogResolverOptio
 		}
 		w = f
 	}
+	logger := slog.New(slog.NewTextHandler(w, &slog.HandlerOptions{
+		ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
+			if a.Key == "msg" || a.Key == "level" {
+				return slog.Attr{}
+			}
+			return a
+		},
+	}))
 	return &QueryLogResolver{
 		resolver: resolver,
-		w:        w,
+		logger:   logger,
 	}, nil
 }
 
 // Resolve logs the query details and passes the query to the next resolver.
 func (r *QueryLogResolver) Resolve(q *dns.Msg, ci ClientInfo) (*dns.Msg, error) {
-	log := logger(r.id, q, ci)
 	question := q.Question[0]
-	now := time.Now().Format(time.RFC3339Nano)
-	r.mu.Lock()
-	_, err := fmt.Fprintf(r.w, "%s %s %s %s %s\n", now, ci.SourceIP, question.Name, dns.Class(question.Qclass).String(), dns.Type(question.Qtype).String())
-	r.mu.Unlock()
-	if err != nil {
-		log.WithError(err).Error("failed to write query to log")
+	attrs := []slog.Attr{
+		slog.String("source-ip", ci.SourceIP.String()),
+		slog.String("question-name", question.Name),
+		slog.String("question-class", dns.Class(question.Qclass).String()),
+		slog.String("question-type", dns.Type(question.Qtype).String()),
 	}
+
+	// Add ECS attributes if present
+	edns0 := q.IsEdns0()
+	if edns0 != nil {
+		// Find the ECS option
+		for _, opt := range edns0.Option {
+			ecs, ok := opt.(*dns.EDNS0_SUBNET)
+			if ok {
+				attrs = append(attrs, slog.String("ecs-addr", ecs.Address.String()))
+			}
+		}
+	}
+
+	r.logger.LogAttrs(context.Background(), slog.LevelInfo, "", attrs...)
 	return r.resolver.Resolve(q, ci)
 }
 
