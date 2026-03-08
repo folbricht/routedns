f8d530169241544ab145a6c07c7fcb3df7f22038
Anuskuss <anuskuss@googlemail.com>
2026-03-08 04:44:04 +0100

    Don't set `ra` if the answer was generated, additionally set `aa` if the answer is static

--- blocklist.go
+++ blocklist.go
@@ -150,7 +150,7 @@ func (r *Blocklist) Resolve(q *dns.Msg, ci ClientInfo) (*dns.Msg, error) {
 
 	answer := new(dns.Msg)
 	answer.SetReply(q)
-	answer.RecursionAvailable = q.RecursionDesired
+	answer.RecursionAvailable = false // we didn't do any recursion
 
 	// We have an IP address to return, make sure it's of the right type. If not return NXDOMAIN.
 	var spoof []dns.RR
