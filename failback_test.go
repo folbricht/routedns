bb2f75c90e641e55d54f1edfba736752db8c7849
Frank Olbricht <frank.olbricht@gmail.com>
2026-03-12 09:16:40 +0100

    Add tests for isSuccessResponse() (#500)
    * Add tests for isSuccessResponse() in FailBack
    
    Tests cover all branches of the refactored isSuccessResponse logic:
    - nil response handling
    - SERVFAIL with/without ServfailError option
    - REFUSED and NOTIMP always trigger failure
    - NXDOMAIN passes through as success
    - EmptyError disabled short-circuits to success
    - Matching/non-matching answer types (A, CNAME, SOA)
    - CNAME chain with trailing match
    - EDE blocked/censored/filtered treated as deliberate empty (success)
    - Other EDE codes treated as failure
    - TypeANY with single HINFO treated as failure
    
    * handle nxdomain with EmptyError
    
    * more tests

--- failback_test.go
+++ failback_test.go
@@ -130,3 +130,206 @@ func TestFailBackServfailOKOption(t *testing.T) {
 	require.NotEqual(t, dns.RcodeServerFailure, a.Rcode)
 	require.Equal(t, 1, goodResolver.hitCount)
 }
+
+func TestFailBackIsSuccessResponse(t *testing.T) {
+	newFB := func(opts FailBackOptions) *FailBack {
+		r := new(TestResolver)
+		return NewFailBack("test-fb", opts, r)
+	}
+
+	newMsg := func(qtype uint16, rcode int) *dns.Msg {
+		a := new(dns.Msg)
+		a.SetQuestion("test.com.", qtype)
+		a.Rcode = rcode
+		return a
+	}
+
+	addAnswer := func(msg *dns.Msg, rrtype uint16) {
+		switch rrtype {
+		case dns.TypeA:
+			msg.Answer = append(msg.Answer, &dns.A{
+				Hdr: dns.RR_Header{Name: "test.com.", Rrtype: dns.TypeA, Class: dns.ClassINET, Ttl: 300},
+			})
+		case dns.TypeCNAME:
+			msg.Answer = append(msg.Answer, &dns.CNAME{
+				Hdr:    dns.RR_Header{Name: "test.com.", Rrtype: dns.TypeCNAME, Class: dns.ClassINET, Ttl: 300},
+				Target: "other.com.",
+			})
+		case dns.TypeHINFO:
+			msg.Answer = append(msg.Answer, &dns.HINFO{
+				Hdr: dns.RR_Header{Name: "test.com.", Rrtype: dns.TypeHINFO, Class: dns.ClassINET, Ttl: 300},
+			})
+		case dns.TypeSOA:
+			msg.Answer = append(msg.Answer, &dns.SOA{
+				Hdr: dns.RR_Header{Name: "test.com.", Rrtype: dns.TypeSOA, Class: dns.ClassINET, Ttl: 300},
+			})
+		}
+	}
+
+	addEDE := func(msg *dns.Msg, code uint16) {
+		opt := msg.IsEdns0()
+		if opt == nil {
+			msg.SetEdns0(4096, false)
+			opt = msg.IsEdns0()
+		}
+		opt.Option = append(opt.Option, &dns.EDNS0_EDE{InfoCode: code})
+	}
+
+	t.Run("NilResponse", func(t *testing.T) {
+		fb := newFB(FailBackOptions{})
+		require.True(t, fb.isSuccessResponse(nil))
+	})
+
+	t.Run("NilResponseWithEmptyError", func(t *testing.T) {
+		fb := newFB(FailBackOptions{EmptyError: true})
+		require.True(t, fb.isSuccessResponse(nil))
+	})
+
+	t.Run("ServfailWithoutOption", func(t *testing.T) {
+		fb := newFB(FailBackOptions{ServfailError: false})
+		msg := newMsg(dns.TypeA, dns.RcodeServerFailure)
+		require.True(t, fb.isSuccessResponse(msg))
+	})
+
+	t.Run("ServfailWithOption", func(t *testing.T) {
+		fb := newFB(FailBackOptions{ServfailError: true})
+		msg := newMsg(dns.TypeA, dns.RcodeServerFailure)
+		require.False(t, fb.isSuccessResponse(msg))
+	})
+
+	t.Run("Refused", func(t *testing.T) {
+		fb := newFB(FailBackOptions{})
+		msg := newMsg(dns.TypeA, dns.RcodeRefused)
+		require.False(t, fb.isSuccessResponse(msg))
+	})
+
+	t.Run("NotImplemented", func(t *testing.T) {
+		fb := newFB(FailBackOptions{})
+		msg := newMsg(dns.TypeA, dns.RcodeNotImplemented)
+		require.False(t, fb.isSuccessResponse(msg))
+	})
+
+	t.Run("NXDomain", func(t *testing.T) {
+		fb := newFB(FailBackOptions{})
+		msg := newMsg(dns.TypeA, dns.RcodeNameError)
+		require.True(t, fb.isSuccessResponse(msg))
+	})
+
+	t.Run("NXDomainWithEmptyError", func(t *testing.T) {
+		fb := newFB(FailBackOptions{EmptyError: true})
+		msg := newMsg(dns.TypeA, dns.RcodeNameError)
+		require.True(t, fb.isSuccessResponse(msg))
+	})
+
+	t.Run("EmptyErrorDisabled", func(t *testing.T) {
+		fb := newFB(FailBackOptions{EmptyError: false})
+		msg := newMsg(dns.TypeA, dns.RcodeSuccess)
+		require.True(t, fb.isSuccessResponse(msg))
+	})
+
+	t.Run("MatchingAnswerType", func(t *testing.T) {
+		fb := newFB(FailBackOptions{EmptyError: true})
+		msg := newMsg(dns.TypeA, dns.RcodeSuccess)
+		addAnswer(msg, dns.TypeA)
+		require.True(t, fb.isSuccessResponse(msg))
+	})
+
+	t.Run("MatchingAnswerAfterCNAME", func(t *testing.T) {
+		fb := newFB(FailBackOptions{EmptyError: true})
+		msg := newMsg(dns.TypeA, dns.RcodeSuccess)
+		addAnswer(msg, dns.TypeCNAME)
+		addAnswer(msg, dns.TypeA)
+		require.True(t, fb.isSuccessResponse(msg))
+	})
+
+	t.Run("OnlyCNAMEsNoMatch", func(t *testing.T) {
+		fb := newFB(FailBackOptions{EmptyError: true})
+		msg := newMsg(dns.TypeA, dns.RcodeSuccess)
+		addAnswer(msg, dns.TypeCNAME)
+		require.False(t, fb.isSuccessResponse(msg))
+	})
+
+	t.Run("CNAMEQueryWithCNAMEAnswer", func(t *testing.T) {
+		fb := newFB(FailBackOptions{EmptyError: true})
+		msg := newMsg(dns.TypeCNAME, dns.RcodeSuccess)
+		addAnswer(msg, dns.TypeCNAME)
+		require.True(t, fb.isSuccessResponse(msg))
+	})
+
+	t.Run("OnlySOANoMatch", func(t *testing.T) {
+		fb := newFB(FailBackOptions{EmptyError: true})
+		msg := newMsg(dns.TypeA, dns.RcodeSuccess)
+		addAnswer(msg, dns.TypeSOA)
+		require.False(t, fb.isSuccessResponse(msg))
+	})
+
+	t.Run("EmptyAnswerNoEDE", func(t *testing.T) {
+		fb := newFB(FailBackOptions{EmptyError: true})
+		msg := newMsg(dns.TypeA, dns.RcodeSuccess)
+		require.False(t, fb.isSuccessResponse(msg))
+	})
+
+	t.Run("EmptyAnswerWithEDEBlocked", func(t *testing.T) {
+		fb := newFB(FailBackOptions{EmptyError: true})
+		msg := newMsg(dns.TypeA, dns.RcodeSuccess)
+		addEDE(msg, dns.ExtendedErrorCodeBlocked)
+		require.True(t, fb.isSuccessResponse(msg))
+	})
+
+	t.Run("EmptyAnswerWithEDECensored", func(t *testing.T) {
+		fb := newFB(FailBackOptions{EmptyError: true})
+		msg := newMsg(dns.TypeA, dns.RcodeSuccess)
+		addEDE(msg, dns.ExtendedErrorCodeCensored)
+		require.True(t, fb.isSuccessResponse(msg))
+	})
+
+	t.Run("EmptyAnswerWithEDEFiltered", func(t *testing.T) {
+		fb := newFB(FailBackOptions{EmptyError: true})
+		msg := newMsg(dns.TypeA, dns.RcodeSuccess)
+		addEDE(msg, dns.ExtendedErrorCodeFiltered)
+		require.True(t, fb.isSuccessResponse(msg))
+	})
+
+	t.Run("EmptyAnswerWithOtherEDE", func(t *testing.T) {
+		fb := newFB(FailBackOptions{EmptyError: true})
+		msg := newMsg(dns.TypeA, dns.RcodeSuccess)
+		addEDE(msg, dns.ExtendedErrorCodeOther)
+		require.False(t, fb.isSuccessResponse(msg))
+	})
+
+	t.Run("EmptyAnswerWithEDEProhibited", func(t *testing.T) {
+		fb := newFB(FailBackOptions{EmptyError: true})
+		msg := newMsg(dns.TypeA, dns.RcodeSuccess)
+		addEDE(msg, dns.ExtendedErrorCodeProhibited)
+		require.False(t, fb.isSuccessResponse(msg))
+	})
+
+	t.Run("EmptyAnswerWithInvalidEDE", func(t *testing.T) {
+		fb := newFB(FailBackOptions{EmptyError: true})
+		msg := newMsg(dns.TypeA, dns.RcodeSuccess)
+		addEDE(msg, 9999)
+		require.False(t, fb.isSuccessResponse(msg))
+	})
+
+	t.Run("TypeANYWithHINFO", func(t *testing.T) {
+		fb := newFB(FailBackOptions{EmptyError: true})
+		msg := newMsg(dns.TypeANY, dns.RcodeSuccess)
+		addAnswer(msg, dns.TypeHINFO)
+		require.False(t, fb.isSuccessResponse(msg))
+	})
+
+	t.Run("TypeANYWithHINFOAndA", func(t *testing.T) {
+		fb := newFB(FailBackOptions{EmptyError: true})
+		msg := newMsg(dns.TypeANY, dns.RcodeSuccess)
+		addAnswer(msg, dns.TypeHINFO)
+		addAnswer(msg, dns.TypeA)
+		require.True(t, fb.isSuccessResponse(msg))
+	})
+
+	t.Run("TypeANYWithRealAnswer", func(t *testing.T) {
+		fb := newFB(FailBackOptions{EmptyError: true})
+		msg := newMsg(dns.TypeANY, dns.RcodeSuccess)
+		addAnswer(msg, dns.TypeA)
+		require.True(t, fb.isSuccessResponse(msg))
+	})
+}
